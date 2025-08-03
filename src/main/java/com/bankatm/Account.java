fdcd5d8961c6a8871cc34f84ceef547f5df2e64e
Laisya <cakalemre25@gmail.com>
2025-08-03 12:01:53 +0300

    feat: create Account class with initial properties and constructor

---
+++ src/main/java/com/bankatm/Account.java
@@ -0,0 +1,17 @@
+package com.bankatm;
+
+public class Account {
+
+    private String accountNumber;
+    private String pin;
+    private double balance;
+
+    public Account(String accountNumber, String pin, double balance) {
+        this.accountNumber = accountNumber;
+        this.pin = pin;
+        this.balance = balance;
+    }
+
+
+
+}
