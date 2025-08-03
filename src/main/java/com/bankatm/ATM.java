9b800f78da0dc16bf5145fe5e120245e403570f5
Laisya <cakalemre25@gmail.com>
2025-08-03 17:52:55 +0300

    feat: add ATM class and application entry point

---
+++ src/main/java/com/bankatm/ATM.java
@@ -0,0 +1,16 @@
+package com.bankatm;
+import java.util.Scanner;
+
+public class ATM {
+    private Scanner scanner;
+    private Bank bank;
+
+    public ATM() {
+        this.scanner = new Scanner(System.in);
+        this.bank = new Bank();
+    }
+
+    public void run (){
+
+    }
+}
--- src/main/java/com/bankatm/App.java
+++ src/main/java/com/bankatm/App.java
@@ -1,5 +1,10 @@
 package com.bankatm;
 
 public class App {
+    public static void main(String[] args) {
+
+        ATM atm = new ATM();
+        atm.run();
+    }
 
 }
